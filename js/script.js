453ba402a723b513c9f40f1d2f0838d7e6d1e1df
YourChester <artyrkamenev@gmail.com>
2019-03-19 20:44:25 +0300

    First day

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const main = document.querySelector('.main')
+const addList = document.querySelector('.button')
+
+const name = 'today'
+const arr = ['buy cookis', 'clean my room', 'cook diner', 'do homework']
+
+function createToDoList(arr, name)
+{
+    const divList = document.createElement('div')
+    const divHeader = document.createElement('div')
+    divHeader.innerHTML = `<h2>${name}</h2>`
+    divList.appendChild(divHeader)
+    divHeader.classList.add('div_header')
+    divList.classList.add('div_list')
+    arr.map((item) => {
+        const p = document.createElement('p')
+        p.textContent = item
+        divList.appendChild(p)
+    })
+    main.appendChild(divList)
+}
+
+addList.addEventListener('click', () => {
+    console.log("add todo list")
+})
+
+createToDoList(arr,name)
+createToDoList(arr,name)
