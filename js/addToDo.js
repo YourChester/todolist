f26ecc234a4610b273b5ac3fe3acb39bc7c16218
Your <artyrkamenev@gmail.com>
2019-03-21 22:05:40 +0300

    third commit

--- js/addToDo.js
+++ js/addToDo.js
@@ -1,6 +1,9 @@
 const addBtn = document.querySelector('#add')
 const addToDo = document.querySelector('.addToDo')
 const addToDo_btn = document.querySelector('.addToDo_btn')
+const input_btn = document.querySelector('.input_btn')
+let textArr = []
+let header = ""
 
 let n = 1
 
@@ -8,6 +11,7 @@ addBtn.addEventListener('click', () => {
     if(n <= 10) {
         const input = document.createElement('input')
         input.setAttribute('type', 'text')
+        input.setAttribute('id', `todo_${n}`)
         input.setAttribute('name', `todo_${n}`)
         input.setAttribute('placeholder', 'What do you do')
         input.classList.add('input_text')
@@ -18,3 +22,7 @@ addBtn.addEventListener('click', () => {
         alert("This all! Please stop!")
     }
 })
+
+function goback(){
+    alert("all comleate! go back!")
+}
--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 const main = document.querySelector('.main')
 const addList = document.querySelector('.button')
 
-const name = 'today'
-const arr = ['buy cookis', 'clean my room', 'cook diner', 'do homework']
-
-function createToDoList(arr, name)
+function createToDoList(arr)
 {
-    const divList = document.createElement('div')
-    const divHeader = document.createElement('div')
-    divHeader.innerHTML = `<h2>${name}</h2>`
-    divList.appendChild(divHeader)
-    divHeader.classList.add('div_header')
-    divList.classList.add('div_list')
-    arr.map((item) => {
-        const p = document.createElement('p')
-        p.textContent = item
-        divList.appendChild(p)
-    })
-    main.appendChild(divList)
+    const length = arr.head.length
+    for(let i = 0; i < length; i++){
+        const divList = document.createElement('div')
+        const divHeader = document.createElement('div')
+        divHeader.innerHTML = `<h2>${arr.head[i]}</h2>`
+        divList.appendChild(divHeader)
+        divHeader.classList.add('div_header')
+        divList.classList.add('div_list')
+        arr.text[i].map((item) => {
+            const p = document.createElement('p')
+            p.textContent = item
+            divList.appendChild(p)
+        })
+        main.appendChild(divList)
+    }
 }
 
 addList.addEventListener('click', () => {
     document.location.href = '/addToDo.html'
 })
 
-createToDoList(arr,name)
-createToDoList(arr,name)
+fetch('js/db.json')
+    .then(response => response.json())
+    .then(json => createToDoList(json.allToDo))
